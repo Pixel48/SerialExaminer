51813e5a968c5213c37fba6a22b58e8e5faea1d5
Pixel48 <piksel48@gmail.com>
2020-05-04 20:52:41 +0200

    Add substructure of substructure of app GUI

--- SerialExaminer.py
+++ SerialExaminer.py
@@ -2,8 +2,18 @@
 # -*- coding: utf-8 -*-
 # SerialExaminer.py
 #   by Pixel
+from tkinter import *
+
+class MainWindow(object):
+  """Creator for main apilcation window"""
+  def __init__(self, master):
+    self.master = master
+    self.frame = Frame(self.master)
+    pass # TODO: Design interface
+
 def main():
-  pass
+  root = Tk()
+  app = MainWindow(root)
 
 if __name__ == '__main__':
   main()
