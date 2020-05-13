c57027ac61e5ca5b5b9353e7e25dae8419c7f0fc
Pixel48 <piksel48@gmail.com>
2020-05-13 23:25:38 +0200

    Set version to v0.1.4

--- SerialExaminer.py
+++ SerialExaminer.py
@@ -8,7 +8,7 @@ from tkinter import filedialog
 import tkinter.font as tkFont
 import os, pickle#, shutil, csv # NOTE: not used yet
 
-versionTag = 'v0.1.3'
+versionTag = 'v0.1.4'
 
 # SOME GLOBALS
 R = 0
