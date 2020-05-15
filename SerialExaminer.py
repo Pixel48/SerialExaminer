f733384bc45087ba7636e2263ba73e6a6309c3e7
Pixel48 <piksel48@gmail.com>
2020-05-15 20:22:30 +0200

    Recenter label in 'SeriEx Key Creator' window

--- SerialExaminer.py
+++ SerialExaminer.py
@@ -217,7 +217,7 @@ class KeyCreatorWindow(object):
     self.mainFont = tkFont.Font(size = 14)
     self.mainLabel = Label(frame, font = self.mainFont)
     self.mainLabel['text'] = "Serial Examiner\nKey Creator"
-    self.mainLabel.grid(row = R, column = C, columnspan = 6)
+    self.mainLabel.grid(row = R, column = C, columnspan = 7)
     # question quantity #
     # label
     newRow()
